b9c9afbffc232ce9a19e380018544b4594ede9e0
dsj_lan_bing <lb@gdysj.com.cn>
2017-07-10 18:13:25 +0800

    ok

--- app/src/main/java/com/bing/lan/share/MainActivity.java
+++ app/src/main/java/com/bing/lan/share/MainActivity.java
@@ -1,7 +1,6 @@
 package com.bing.lan.share;
 
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -33,31 +32,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //  增加  显眼的提示
+
     private void showShare() {
         String url = "http://www.mob.com/downloadDetail/ShareSDK/android";
-        String url1 = "http://www.mobc.com/downloadDetail/ShareSDK/android";
-        String url2 = "http://www.mobb.com/downloadDetail/ShareSDK/android";
-        String url3 = "http://www.moba.com/downloadDetail/ShareSDK/android";
-
+        String url2 = "http://news.3news.cn/html/jin/2017/0710/64380.html";
+        String url3 = "https://www.baidu.com/";
 
         OnekeyShare oks = new OnekeyShare();
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
 
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
-        oks.setTitle("标题");
-
+        oks.setTitle("我是分享标题");
         // titleUrl是标题的网络链接，QQ和QQ空间等使用
         oks.setTitleUrl(url3);
 
         // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
+        oks.setText("我是分享文本我是分享文本我是分享文本我是分享文本我是分享文本");
 
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setImagePath(Environment.getExternalStorageDirectory().getAbsolutePath() + "/test.jpg");//确保SDcard下面存在此张图片
+        //oks.setImagePath(Environment.getExternalStorageDirectory().getAbsolutePath() + "/test.jpg");//确保SDcard下面存在此张图片
 
-        // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl(url );//必须要是安全域名的子域名
+        // url仅在微信（包括好友和朋友圈），新浪微博  中使用
+        // 新浪微博  必须要是安全域名的子域名，不能短时间内发送同一条微博
+        // 微信好友 微信收藏 朋友圈 中 必须 带上图片才能点开，但是新浪微博不需要
+        oks.setUrl(url);
+
+
 
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
         oks.setComment("我是测试评论文本");
@@ -66,9 +68,7 @@ public class MainActivity extends AppCompatActivity {
         oks.setSite(getString(R.string.app_name));
 
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl(url2 );
-
-
+        oks.setSiteUrl(url2);
 
         // 启动分享GUI
         oks.show(this);
